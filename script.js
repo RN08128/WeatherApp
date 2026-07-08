9038131c19ae038f4bfffe7f617c0f47df0f36a6
RN08128 <watitsantos@gmail.com>
2026-07-08 17:51:30 -0300

    Launching WeatherApp

---
+++ script.js
@@ -0,0 +1,116 @@
+const apiKey = '32e0aa13e23ccc8ac279a05ca45d773e';
+
+const cityInput = document.getElementById("cidade-input");
+const searchBtn = document.getElementById("buscar-button");
+const menuBox = document.getElementById("menu-box");
+
+const cityName = document.getElementById("city-name");
+const temperature = document.getElementById("temperature");
+const weatherIcon = document.getElementById("weather-icon");
+const weatherDescription = document.getElementById("weather-description");
+const wind = document.getElementById("wind");
+const humidity = document.getElementById("humidity");
+
+const errorBox = document.getElementById("error-box");
+const errorMessage = document.getElementById("error-message");
+const weatherBox = document.getElementById("weather-box");
+
+const clicksnd = document.createElement("audio");
+clicksnd.src = "sound/clicksound.mp3";
+
+async function checkWeather(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`;
+
+    try {
+        const response = await fetch(url)
+
+        const data = await response.json();
+        updateWeatherTheme(data);
+        console.log(data);
+
+        weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+
+        cityName.innerText = `${data.name}, ${data.sys.country}`;
+
+        let temp = data.main.temp;
+        temperature.innerText = temp.toFixed(0) + "°C";
+
+        weatherDescription.innerText = `${data.weather[0].description}`;
+
+        humidity.innerText = `${data.main.humidity}%`;
+
+        let windvalue = (data.wind.speed) * 3.6;
+        wind.innerText = windvalue.toFixed(1) + " km/h";
+
+        cityInput.value = "";
+        cityInput.focus;
+    } catch (error) {
+        console.error("Erro ao buscar dados do clima", error)
+        alert("Erro ao buscar! Verifique a conexão ou tente novamente mais tarde")
+
+        weatherIcon.src = `https://openweathermap.org/img/wn/01d@2x.png`
+        cityName.innerText = `Brasil, BR`;
+        temperature.innerText = "35°C";
+        weatherDescription.innerText = `céu limpo`;
+        humidity.innerText = `30%`;
+        wind.innerText = "6 km/h";
+        menuBox.classList.add("sunny");
+        document.body.classList.add("sunny");
+    }
+}
+
+function updateWeatherTheme(data) {
+    document.body.classList.remove("sunny", "rainy", "cloudy", "night");
+    menuBox.classList.remove("sunny", "rainy", "cloudy", "night")
+
+    const mainCondition = data.weather[0].main;
+    const iconCode = data.weather[0].icon;
+
+    //noite
+    if (iconCode.endsWith("n")) {
+        document.body.classList.add("night");
+        menuBox.classList.add("night")
+        return;
+    }
+
+    //dia
+    switch (mainCondition) {
+        case "Clear":
+            document.body.classList.add("sunny");
+            menuBox.classList.add("sunny")
+            break;
+        case "Rain":
+        case "Drizzle":
+        case "Thunderstorm":
+            document.body.classList.add("rainy");
+            menuBox.classList.add("rainy")
+            break;
+        case "Clouds":
+            document.body.classList.add("cloudy");
+            menuBox.classList.add("cloudy")
+            break;
+        default:
+            document.body.classList.add("sunny");
+            menuBox.classList.add("sunny")
+            break;
+    }
+}
+
+searchBtn.addEventListener("click", () => {
+    clicksnd.play();
+    if (cityInput.value.trim() !== "") {
+        checkWeather(cityInput.value);
+    } else {
+        alert("Insira um nome válido!");
+    }
+});
+
+cityInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter" && cityInput.value.trim() !== "") {
+        clicksnd.play();
+        checkWeather(cityInput.value);
+    } else if (event.key === "Enter") {
+        clicksnd.play();
+        alert("Insira um nome válido!");
+    }
+});
